f1a9ad020a474579d9ffea2f855109b78d727c12
Susim <susim.samanta@me.com>
2014-01-24 10:05:32 +0530

    Initial Commit

---
+++ Fun/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Fun
+//
+//  Created by Susim Samanta on 24/01/14.
+//  Copyright (c) 2014 SusimSamanta. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
